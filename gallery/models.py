f178dc81aa3803d3d494b23e4c013aec44ff62c4
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-01 23:16:08 +0300

    defining image class

--- gallery/models.py
+++ gallery/models.py
@@ -7,4 +7,9 @@ class Poster(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
 
-    
+class image(models.Model):
+     image = models.CharField(max_length=255)
+     title = models.CharField(max_length=255) 
+     description = models.CharField(max_length=255)
+     
+     
