1b4afc803ba7ae6f8ccca5f655693626a9a5552b
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-01 23:13:59 +0300

    defining poster class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Poster(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    
