20c8be6ab2521bceeeaa7c4960a8a0f50578d2fd
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-02 00:08:18 +0300

    updating model class

--- gallery/models.py
+++ gallery/models.py
@@ -3,13 +3,41 @@ from django.db import models
 # Create your models here.
 
 class Poster(models.Model):
-    first_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255 ,primary_key = True)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
 
-class image(models.Model):
-     image = models.CharField(max_length=255)
+    def __str__(self):
+        return self.first_name
+
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255,primary_key = True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255,primary_key = True) 
+
+    def __str__(self):
+        return self.name   
+
+class Image(models.Model):
+     image = models.CharField(max_length=255,primary_key = True)
      title = models.CharField(max_length=255) 
      description = models.CharField(max_length=255)
+     date = models.CharField(max_length=255,null=True)
+     location = models.ForeignKey(Location,on_delete=models.CASCADE)
+     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+     def __str__(self):
+         return self.image
+
+
+
+
      
-     
+
+
