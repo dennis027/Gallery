11126835bb6583cd612757dd13332914af1b2a7e
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-01 13:09:20 +0300

    creating template inheritance with base.html

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,9 @@
 from django.http  import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+
+def gallery(request):
+    return render(request,'all-gallery/gallery.html')    
