e31d5a0c226b4f0e8bb4330e97b71210a49ecc91
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-03 15:45:28 +0300

    accessing first variables

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,11 @@
 from django.http  import HttpResponse
 from django.shortcuts import render,redirect
-from .mod
+from .models import Poster,Category,Location,Image
 
 # Create your views here.
 def welcome(request):
-    return render(request,'all-gallery/gallery.html')
+    images= Image.objects.all()
+    return render(request,'all-gallery/gallery.html',{'images':images})
 
 # def gallery(request):
 #     return render(request,'all-gallery/gallery.html')    
